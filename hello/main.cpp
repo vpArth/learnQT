3f326cc2d3434a35ee495273bcf78f7897469373
Alexander Deider <for_inbox@mail.ru>
2016-08-17 02:44:07 +0700

    Hello, World. QDialog.

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
+#include <QDialog>
 #include <QLabel>
 
 int main(int argc, char* argv[]) {
-    QApplication a(argc, argv);
-    QLabel l("Hello, World!");
-    l.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    QDialog *dialog = new QDialog;
+    QLabel *label = new QLabel(dialog);
+    label->setText("<font color=red>Hello, World!</font>");
+    dialog->show();
+
+    int ret = app.exec();
+
+    delete label;
+    delete dialog;
+    return ret;
 }
